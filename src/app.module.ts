e61520e16607eb961af9bf1f0e1262256231cb08
innerstella <suji0509@naver.com>
2026-01-31 22:42:22 +0900

    study: movies controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
 
 // decorator : 클래스 위의 함수
 @Module({
   imports: [],
-  controllers: [AppController], // url을 가져오고 함수를 실행함 (express의 router와 같은 역할)
-  providers: [AppService], 
+  controllers: [MoviesController], // url을 가져오고 함수를 실행함 (express의 router와 같은 역할)
+  providers: [], 
 })
 export class AppModule {} // App module은 루트 모듈
---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+
+@Controller('movies') // NOTE: entry point
+export class MoviesController {
+    // router
+    @Get()
+    getAll(){
+        return 'This will return all movies';
+    }
+
+    // prameter 가져오기
+    @Get('/:id')
+    getOne(@Param('id') movieId: string){
+        return `This will return one movie with the id: ${movieId}`;
+    }
+
+    @Post()
+    create(){
+        return 'This will create a movie';
+    }
+
+    @Delete('/:id')
+    remove(@Param('id') movieId: string){
+        return `This will delete a movie with the id: ${movieId}`;
+    }
+
+    @Patch('/:id')
+    patch(@Param('id') movieId: string){
+        return `This will patch a movie with the id: ${movieId}`;
+    }
+}
