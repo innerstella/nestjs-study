1a11f2624b64b0cd3ccffee4ec4dd28a36613f15
innerstella <suji0509@naver.com>
2026-02-01 23:40:50 +0900

    study: query params and body

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies') // NOTE: entry point
 export class MoviesController {
@@ -8,24 +8,32 @@ export class MoviesController {
         return 'This will return all movies';
     }
 
+    // search 부분이 get보다 밑에 있으면 NestJS는 search를 id로 판단함
+    @Get("search")
+    search(@Query('year') searchingYear: string){
+        return `We are searching for a movie title made after ${searchingYear}.`;
+    }
+
     // prameter 가져오기
-    @Get('/:id')
+    @Get(':id')
     getOne(@Param('id') movieId: string){
         return `This will return one movie with the id: ${movieId}`;
     }
-
+    
     @Post()
-    create(){
-        return 'This will create a movie';
+    create(@Body() movieData: any){ // body 가져오기
+        console.log(movieData);
+        return movieData;
     }
 
-    @Delete('/:id')
+    @Delete(':id')
     remove(@Param('id') movieId: string){
         return `This will delete a movie with the id: ${movieId}`;
     }
 
-    @Patch('/:id')
+    @Patch(':id')
     patch(@Param('id') movieId: string){
         return `This will patch a movie with the id: ${movieId}`;
     }
+
 }
