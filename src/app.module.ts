2ed7feb358ab99683345e46bbe76f56b646947d8
innerstella <suji0509@naver.com>
2026-02-02 22:21:21 +0900

    study: service, entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 // decorator : 클래스 위의 함수
 @Module({
   imports: [],
   controllers: [MoviesController], // url을 가져오고 함수를 실행함 (express의 router와 같은 역할)
-  providers: [], 
+  providers: [MoviesService], 
 })
 export class AppModule {} // App module은 루트 모듈
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,34 +1,31 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 
 @Controller('movies') // NOTE: entry point
 export class MoviesController {
+    constructor(private readonly moviesService: MoviesService) {}
+    
     // router
     @Get()
-    getAll(){
-        return 'This will return all movies';
-    }
-
-    // search 부분이 get보다 밑에 있으면 NestJS는 search를 id로 판단함
-    @Get("search")
-    search(@Query('year') searchingYear: string){
-        return `We are searching for a movie title made after ${searchingYear}.`;
+    getAll(): Movie[]{
+        return this.moviesService.getAll();
     }
 
     // prameter 가져오기
     @Get(':id')
-    getOne(@Param('id') movieId: string){
-        return `This will return one movie with the id: ${movieId}`;
+    getOne(@Param('id') movieId: string): Movie{
+        return this.moviesService.getOne(movieId);
     }
     
     @Post()
     create(@Body() movieData: any){ // body 가져오기
-        console.log(movieData);
-        return movieData;
+        return this.moviesService.create(movieData);
     }
 
     @Delete(':id')
     remove(@Param('id') movieId: string){
-        return `This will delete a movie with the id: ${movieId}`;
+        return this.moviesService.deleteOne(movieId);
     }
 
     @Patch(':id')
