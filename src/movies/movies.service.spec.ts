34ee2725e4d132b298e418e11da26e973ba3419d
innerstella <suji0509@naver.com>
2026-02-08 23:18:57 +0900

    study: Testing delete and create

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test } from "@nestjs/testing";
 import { MoviesService } from "./movies.service";
+import { NotFoundException } from "@nestjs/common";
 
 describe('MoviesService', () => {
     let service: MoviesService;
@@ -34,6 +35,39 @@ describe('MoviesService', () => {
             });
             const result = service.getOne(1);
             expect(result).toBeDefined();
+        });
+    });
+
+    describe('deleteOne', () => {
+        it('should delete a movie', () => {
+            service.create({
+                title: 'Test Movie',
+                year: 2000,
+                genres: ['test'],
+            });
+            const result = service.deleteOne(1);
+            expect(result).toBeTruthy();
+        });
+
+        it('should return a 404', () => {
+            try {
+                service.deleteOne(999);
+            } catch (e) {
+                expect(e).toBeInstanceOf(NotFoundException);
+            }
+        });
+    });
+
+    describe('create', () => {
+        it('should create a movie', () => {
+            const beforeCreate = service.getAll().length;
+            service.create({
+                title: 'Test Movie',
+                year: 2000,
+                genres: ['test'],
+            });
+            const afterCreate = service.getAll().length;
+            expect(afterCreate).toBeGreaterThan(beforeCreate);
         });
     });
 });
